4c0dee5d2cb946582ee13dbb0c6ba1add5a3703f
sebo2213 <sebo2213.tesla@gmail.com>
2020-02-20 17:57:07 -0600

    possible test cases
    Looking at the tasks required for the server side sign in I started thinking of possible test cases to ensure that the functionality works as needed.  Will be trying to work from the test driven development style

--- src/test/java/edu/uark/registerapp/RegisterappApplicationTests.java
+++ src/test/java/edu/uark/registerapp/RegisterappApplicationTests.java
@@ -13,4 +13,26 @@ public class RegisterappApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testSignInTakeID(){
+		//will test to see if the user name field is actually taking a result
+		//will test that the error checking is working properly when looking for empty names
+	}
+
+	@Test
+	public void testSignInIDTakePassword(){
+		//tests for seeing if password is empty
+		//look to see if the password matches whats in the records
+	}
+
+	@Test
+	public void testSignInUserExists(){
+		//tests the database to see if the user exists
+	}
+
+	@Test
+	public void testSignInQuery(){
+		//tests to see if the sign in actually queries the database
+	}
+
 }
